5e9f1a475b0ea4eea8d465151951a8157fd02203
Huihuihh <tianhui@awcloud.com>
2020-09-28 16:29:59 +0800

    使用opencv人脸识别方法

--- gpu/calculate_dection_face.py
+++ gpu/calculate_dection_face.py
@@ -7,7 +7,8 @@ import sys
 import os
 import copy
 import argparse
-import align.detect_face
+
+faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
 
 def main(args):
     dection()
@@ -26,20 +27,19 @@ def dection():
     images = load_and_align_data(img_path_set, 160, 44)
 
     save_dir='save_img/'
-    
+
     if(os.path.exists(save_dir)==False):
         os.mkdir(save_dir)
-
-    # 改为emb_img文件夹的绝对路径  
-    emb_dir='emb_img/'
-    
-    if(os.path.exists(emb_dir)==False):
-        os.mkdir(emb_dir)
 
     count=0
     for file in os.listdir(img_dir):
         misc.imsave(os.path.join(save_dir,file),images[count])
         count=count+1
+
+    emb_dir='emb_img/'
+
+    if(os.path.exists(emb_dir)==False):
+        os.mkdir(emb_dir)
 
     a = 0
     filename_list = os.listdir(save_dir)
@@ -49,37 +49,21 @@ def dection():
         os.rename(used_name,new_name)
         a += 1
 
-def load_and_align_data(image_paths, image_size, margin):
+def load_and_align_data(image_paths, image_size, margin, gpu_memory_fraction):
 
     minsize = 20 # minimum size of face
     threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
     factor = 0.709 # scale factor
-    
-    print('Creating networks and loading parameters')
-    with tf.Graph().as_default():
-        sess = tf.Session()
-        with sess.as_default():
-            pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
-  
+
     tmp_image_paths=copy.copy(image_paths)
     img_list = []
     for image in tmp_image_paths:
         print(image)
         img = misc.imread(os.path.expanduser(image), mode='RGB')
-        # img = misc.imread(image, mode='RGB')
-        img_size = np.asarray(img.shape)[0:2]
-        bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
-        if len(bounding_boxes) < 1:
-          image_paths.remove(image)
-          print("can't detect face, remove ", image)
-          continue
-        det = np.squeeze(bounding_boxes[0,0:4])
-        bb = np.zeros(4, dtype=np.int32)
-        bb[0] = np.maximum(det[0]-margin/2, 0)
-        bb[1] = np.maximum(det[1]-margin/2, 0)
-        bb[2] = np.minimum(det[2]+margin/2, img_size[1])
-        bb[3] = np.minimum(det[3]+margin/2, img_size[0])
-        cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
+        faces = faceCascade.detectMultiScale(img, scaleFactor=1.2, minNeighbors=5)
+
+        for (x,y,h,w) in faces:
+            cropped= img[y:y+h, x:x+w]
 
         # 根据cropped位置对原图resize，并对新得的aligned进行白化预处理
         aligned = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
@@ -95,12 +79,5 @@ def prewhiten(x):
     y = np.multiply(np.subtract(x, mean), 1/std_adj)
     return y
 
-def parse_arguments(argv):
-    parser=argparse.ArgumentParser()
-    parser.add_argument('--img_size',type=int,default=160)
-
-    return parser.parse_args(argv)
-
-
 if __name__=='__main__':
-    main(parse_arguments(sys.argv[1:]))
+    main()
--- gpu/face_recognition.py
+++ gpu/face_recognition.py
@@ -10,12 +10,12 @@ import sys
 import os
 import copy
 import argparse
-import align.detect_face
 import random
 
 from os.path import join as pjoin
 import matplotlib.pyplot as plt
 from tensorflow.python.platform import gfile
+from time import time
 
 
 import sklearn
@@ -23,14 +23,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn import metrics  
 from sklearn.externals import joblib
+from openvino.inference_engine import IENetwork, IECore
 
+faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
 
 def main():      
 
     with tf.Graph().as_default():
         with tf.Session() as sess:     
             # Load the model 
-            # 这里要改为自己的模型位置
             model='models/facenet.pb'
             model_exp = os.path.expanduser(model)
             with gfile.FastGFile(model_exp,'rb') as f:
@@ -42,7 +43,6 @@ def main():
             images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
             embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
             phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-
 
             image=[]
             nrof_images=0
@@ -66,90 +66,74 @@ def main():
             #video="http://admin:admin@192.168.0.107:8081/"   #此处@后的ipv4 地址需要修改为自己的地址
             # 参数为0表示打开内置摄像头，参数是视频文件路径则打开视频
             #capture =cv2.VideoCapture(video)
-            #capture =cv2.VideoCapture('/home/awcloud/Desktop/tianhui.mp4')
             capture =cv2.VideoCapture(0)
             cv2.namedWindow("camera",1)
+            k = 0
+            count = 3
             timer=0
             while True:
                 ret, frame = capture.read() 
 
-                # rgb frame np.ndarray 480*640*3
-                rgb_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+                if (k == 0):
+                    # rgb frame np.ndarray 480*640*3
+                    rgb_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
                 
-                # 获取 判断标识 bounding_box crop_image
-                mark,bounding_box,crop_image=load_and_align_data(rgb_frame,160,44)
-                timer+=1
-                if(1):
-                    
-                    print(timer)
-                    if(mark):
-                        feed_dict = { images_placeholder: crop_image, phase_train_placeholder:False }
-                        emb = sess.run(embeddings, feed_dict=feed_dict)
-                        temp_num=len(emb)
+                    # 获取 判断标识 bounding_box crop_image
+                    mark,crop_image,faces=load_and_align_data(rgb_frame,160,44)
+                    timer+=1
+                    if(1):
+                        print(timer)
+                        if(mark):
+                            if crop_image is not None:
+                                feed_dict = { images_placeholder: crop_image, phase_train_placeholder:False }
+                                detect_time = []
+                                t0 = time()
+                                emb = sess.run(embeddings, feed_dict=feed_dict)
+                                detect_time.append((time() - t0) * 1000)
+                                print('detect_time is {} ms'.format(np.average(np.asarray(detect_time))))
+                                temp_num=len(emb)
 
-                        fin_obj=[]
-                        print(all_obj)
+                                fin_obj=[]
+                                print(all_obj)
 
-                        # 为bounding_box 匹配标签
-                        for i in range(temp_num):
-                            dist_list=[]
-                            for j in range(compare_num):
-                                dist = np.sqrt(np.sum(np.square(np.subtract(emb[i,:], compare_emb[j,:]))))
-                                dist_list.append(dist)
-                            min_value=min(dist_list)
-                            print(dist_list)
-                            if(min_value>0.65):
-                                fin_obj.append('unknow')
-                            else:
-                                fin_obj.append(all_obj[dist_list.index(min_value)])    
+                                # 为bounding_box 匹配标签
+                                for i in range(temp_num):
+                                    dist_list=[]
+                                    for j in range(compare_num):
+                                        dist = np.sqrt(np.sum(np.square(np.subtract(emb[i], compare_emb[j]))))
+                                        dist_list.append(dist)
+                                    min_value=min(dist_list)
+                                    print(dist_list)
+                                    if(min_value>0.65):
+                                        fin_obj.append('unknow')
+                                    else:
+                                        fin_obj.append(all_obj[dist_list.index(min_value)])    
 
+                                for (x,y,h,w) in faces:
+                                    for rec_position in range(temp_num):
+                                        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+                                        cv2.putText(frame,fin_obj[rec_position],(x,y),cv2.FONT_HERSHEY_COMPLEX_SMALL,1, (128, 128, 0), 2)
 
-                        # 在frame上绘制边框和文字
-                        for rec_position in range(temp_num):                        
-                            cv2.rectangle(frame,(bounding_box[rec_position,0],bounding_box[rec_position,1]),(bounding_box[rec_position,2],bounding_box[rec_position,3]),(0, 255, 0), 2, 8, 0)
-
-                            cv2.putText(
-                                frame,
-                            fin_obj[rec_position], 
-                            (bounding_box[rec_position,0],bounding_box[rec_position,1]),
-                            cv2.FONT_HERSHEY_COMPLEX_SMALL, 
-                            0.8, 
-                            (0, 0 ,255), 
-                            thickness = 2, 
-                            lineType = 2)
-
-                        cv2.imshow('camera',frame)
+                            cv2.imshow('camera',frame)
 
 
-                # cv2.imshow('camera',frame)
-                key = cv2.waitKey(3)
-                if key == 27:
-                    #esc键退出
-                    print("esc break...")
-                    break
+                    key = cv2.waitKey(3)
+                    if key == 27:
+                        #esc键退出
+                        print("esc break...")
+                        break
 
-            # if key == ord(' '):
-            #     # 保存一张图像
-            #     num = num+1
-            #     filename = "frames_%s.jpg" % num
-            #     cv2.imwrite(filename,frame)
-            
-            # When everything is done, release the capture
+                k = k + 1
+                k = k % count
             capture.release()
             cv2.destroyWindow("camera")
-  
+
 # 创建load_and_align_data网络
 print('Creating networks and loading parameters')
 with tf.Graph().as_default():
-    soft_config = tf.ConfigProto(allow_soft_placement=True)
-    soft_config.gpu_options.allow_growth = True
-    sess = tf.Session(config=soft_config)
-    sess.run(tf.global_variables_initializer())
-    with sess.as_default():
-        pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1.0)
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
 
-
-# 修改版load_and_align_data
 # 传入rgb np.ndarray
 def load_and_align_data(img, image_size, margin):
 
@@ -157,40 +141,23 @@ def load_and_align_data(img, image_size, margin):
     threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
     factor = 0.709 # scale factor
 
-    img_size = np.asarray(img.shape)[0:2]
+    faces = faceCascade.detectMultiScale(img, scaleFactor=1.2, minNeighbors=5)
+    if (len(faces) == 0):
+        return 1, None, faces
 
-    # bounding_boxes shape:(1,5)  type:np.ndarray
-    bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+    for (x,y,h,w) in faces:
+        global cropped
+        cropped = img[y:y+h, x:x+w]
 
-    # 如果未发现目标 直接返回
-    if len(bounding_boxes) < 1:
-        return 0,0,0
-
-    # 从数组的形状中删除单维度条目，即把shape中为1的维度去掉
-    # det = np.squeeze(bounding_boxes[:,0:4])
-    det=bounding_boxes
-
-    print('det shape type')
-    print(det.shape)
-    print(type(det))
-
-    det[:,0] = np.maximum(det[:,0]-margin/2, 0)
-    det[:,1] = np.maximum(det[:,1]-margin/2, 0)
-    det[:,2] = np.minimum(det[:,2]+margin/2, img_size[1]-1)
-    det[:,3] = np.minimum(det[:,3]+margin/2, img_size[0]-1)
-
-    det=det.astype(int)
     crop=[]
-    for i in range(len(bounding_boxes)):
-        temp_crop=img[det[i,1]:det[i,3],det[i,0]:det[i,2],:]
-        aligned=misc.imresize(temp_crop, (image_size, image_size), interp='bilinear')
-        prewhitened = prewhiten(aligned)
-        crop.append(prewhitened)
+    aligned=misc.imresize(cropped, (image_size, image_size), interp='bilinear')
+    prewhitened = prewhiten(aligned)
+    crop.append(prewhitened)
 
     # np.stack 将crop由一维list变为二维
     crop_image=np.stack(crop)  
 
-    return 1,det,crop_image
+    return 1,crop_image,faces
 
 def prewhiten(x):
     mean = np.mean(x)
